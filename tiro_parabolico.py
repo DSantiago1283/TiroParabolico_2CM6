59f13a367d7b85f1a3b4aa49d7663cd140d734d7
DSantiago1283 <137456490+DSantiago1283@users.noreply.github.com>
2023-06-23 08:05:45 -0600

    Add files via upload
    Documento de python

--- tiro_parabolico.py
+++ tiro_parabolico.py
@@ -1,33 +1,95 @@
-import math
 import matplotlib.pyplot as plt
+import math
 
 g = 9.81  # Aceleración de la gravedad en m/s^2
 
 def calcular_altura(v0, theta):
-    return (v0 * math.sin(math.radians(theta)))**2 / (2 * g)
+    theta_rad = math.radians(theta)
+    v0y = v0 * math.sin(theta_rad)
+    altura = (v0y ** 2) / (2 * g)
+    return altura
 
-def calcular_alcance_horizontal(v0, theta):
-    return (v0**2 * math.sin(2 * math.radians(theta))) / g
+def calcular_alcance(v0, theta):
+    theta_rad = math.radians(theta)
+    alcance = (v0 ** 2) * math.sin(2 * theta_rad) / g
+    return alcance
 
 def calcular_tiempo_vuelo(v0, theta):
-    return (2 * v0 * math.sin(math.radians(theta))) / g
+    theta_rad = math.radians(theta)
+    tiempo_vuelo = (2 * v0 * math.sin(theta_rad)) / g
+    return tiempo_vuelo
+
+def calcular_velocidad_inicial_horizontal(v0, theta):
+    theta_rad = math.radians(theta)
+    v0x = v0 * math.cos(theta_rad)
+    return v0x
+
+def calcular_velocidad_inicial_vertical(v0, theta):
+    theta_rad = math.radians(theta)
+    v0y = v0 * math.sin(theta_rad)
+    return v0y
+
+def calcular_velocidad_horizontal(v0, theta, t):
+    theta_rad = math.radians(theta)
+    vx = v0 * math.cos(theta_rad)
+    return vx
+
+def calcular_velocidad_vertical(v0, theta, t):
+    vy = calcular_velocidad_inicial_vertical(v0, theta) - g * t
+    return vy
+
+def calcular_posicion_horizontal(x0, v0, theta, t):
+    theta_rad = math.radians(theta)
+    x = x0 + (v0 * math.cos(theta_rad)) * t
+    return x
+
+def calcular_posicion_vertical(y0, v0, theta, t):
+    theta_rad = math.radians(theta)
+    y = y0 + (v0 * math.sin(theta_rad)) * t - 0.5 * g * (t ** 2)
+    return y
 
 def calcular_posicion(x0, y0, v0, theta, t):
-    x = x0 + (v0 * math.cos(math.radians(theta))) * t
-    y = y0 + (v0 * math.sin(math.radians(theta))) * t - (0.5 * g * t**2)
+    theta_rad = math.radians(theta)
+    x = x0 + (v0 * math.cos(theta_rad)) * t
+    y = y0 + (v0 * math.sin(theta_rad)) * t - 0.5 * g * (t ** 2)
     return x, y
 
-def calcular_velocidad_inicial(v0, theta):
-    v0x = v0 * math.cos(math.radians(theta))
-    v0y = v0 * math.sin(math.radians(theta))
-    return v0x, v0y
+def resolver_problema_1():
+    v0 = 30
+    theta = 48
 
-def calcular_velocidad(v0, theta, t):
-    vx = v0 * math.cos(math.radians(theta))
-    vy = v0 * math.sin(math.radians(theta)) - g * t
-    return vx, vy
+    altura = calcular_altura(v0, theta)
+    alcance = calcular_alcance(v0, theta)
+    tiempo_vuelo = calcular_tiempo_vuelo(v0, theta)
 
-def resolver_problema():
+    print("Problema:")
+    print("Un jugador de Fútbol Americano patea el balón con una velocidad de 30 m/s,")
+    print("y éste mismo lleva un ángulo de elevación de 48° respecto a la horizontal.")
+    print("Calcule:")
+    print("a) Altura")
+    print("b) Alcance")
+    print("c) Tiempo que permanece en el aire")
+
+    print(f"\nAltura: {altura} m")
+    print(f"Alcance: {alcance} m")
+    print(f"Tiempo de vuelo: {tiempo_vuelo} s")
+
+    # Generar gráfica
+    t_values = [0]
+    dt = tiempo_vuelo / 100  # Dividimos el tiempo de vuelo en 100 intervalos
+    while t_values[-1] < tiempo_vuelo:
+        t_values.append(t_values[-1] + dt)
+
+    x_values = [calcular_posicion_horizontal(0, v0, theta, t) for t in t_values]
+    y_values = [calcular_posicion_vertical(0, v0, theta, t) for t in t_values]
+
+    plt.plot(x_values, y_values)
+    plt.xlabel("Distancia (m)")
+    plt.ylabel("Altura (m)")
+    plt.title("Tiro Parabólico")
+    plt.show()
+
+def resolver_problema_2():
     print("Resolver problema de tiro parabólico:")
     print("1. Calcular altura")
     print("2. Calcular alcance horizontal")
@@ -44,7 +106,7 @@ def resolver_problema():
     elif opcion == 2:
         v0 = float(input("Ingrese la velocidad inicial (m/s): "))
         theta = float(input("Ingrese el ángulo de elevación (grados): "))
-        alcance = calcular_alcance_horizontal(v0, theta)
+        alcance = calcular_alcance(v0, theta)
         print(f"El alcance horizontal es: {alcance} metros")
 
     elif opcion == 3:
@@ -76,4 +138,16 @@ def resolver_problema():
     else:
         print("Opción inválida. Por favor, seleccione una opción válida.")
 
-resolver_problema()
+# Main
+print("Bienvenido al solucionador de problemas de tiro parabólico")
+
+resolver = input("¿Desea resolver el Problema 1 o el Problema 2? (1/2): ")
+
+if resolver == "1":
+    resolver_problema_1()
+elif resolver == "2":
+    resolver_problema_2()
+else:
+    print("Opción inválida")
+
+1
